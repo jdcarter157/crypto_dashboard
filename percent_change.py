389a1b251cc03fc535b90df7d2ddfa6a24c3aa01
Jordan <85311538+jdcarter157@users.noreply.github.com>
2022-11-21 14:32:41 -0500

    Update percent_change.py
    Signed-off-by: Jordan  <85311538+jdcarter157@users.noreply.github.com>

--- percent_change.py
+++ percent_change.py
@@ -1,10 +1,7 @@
-
-
 from sklearn.linear_model import LinearRegression
 
 
 import dash
-# from dash import html, dcc, callback, Input, Output
 
 
 import plotly.graph_objects as go
@@ -16,160 +13,279 @@ import numpy as np
 import math
 import time
 from datetime import datetime
-now = datetime.now()
+import mysql
 import mysql.connector
+
+
 
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 
-#DB CONN
+import random
+
+# now = datetime.now()
+
+ # DB CONN
+# mydb = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="",
+#
+#     database="ecoins"
+# )
 mydb = mysql.connector.connect(
-   host="localhost",
-   user="root",
-   password="",
-   database="ecoins"
+    host="192.168.0.122",
+    user="crypto",
+    password="crypto",
+    auth_plugin="mysql_native_password",
+    database="coindata"
 )
-#grabbing slopes for each coin to display onto dash
-def bltc_slope():
-    mycursor = mydb.cursor()
-    # QUERY TO GRAB COIN
-    mycursor.execute("SELECT * from coin_data WHERE cname = 'BITMEX:BLTC'")
-    myresult = mycursor.fetchall()
-    # CREATING DF
-    pd1 = pd.DataFrame(myresult, columns=['crid', 'cname', 'cdate', 'price', 'lows', 'highs'])
-    # line
-    # CDATE IS OUR INDEPENDENT VAR
-    pd1['cdate'] = pd.to_datetime(pd1['cdate'])
-    # CHANGING DATE FORMAT TO ORDINAL
-    pd1['cdate'] = pd1['cdate'].apply(lambda x: x.toordinal())
-    # RESHAPING DATA FOR LIN-REG
-    x = pd1['cdate'].to_numpy().reshape((-1, 1))
-    # PRICE IS OUR DEPENDENT VAR
-    y = pd1['price'].to_numpy()
-    # CHOOSING A MODEL
-    model = LinearRegression()
-    # FITTING THE DATA TO MODEL
-    model.fit(x, y)
-    # MAKING SLOPE GLOBAL SO ALL 3 CAN BE DISPLAYED ON THIS PAGE
-    global slope_bltc
-    slope_bltc=str(model.coef_)
-
-
-def eth_slope():
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * from coin_data WHERE cname = 'BITMEX:ETHUSD_ETH'")
-    myresult = mycursor.fetchall()
-    pd1 = pd.DataFrame(myresult, columns=['crid', 'cname', 'cdate', 'price', 'lows', 'highs'])
-    # line
-    pd1['cdate'] = pd.to_datetime(pd1['cdate'])
-    pd1['cdate'] = pd1['cdate'].apply(lambda x: x.toordinal())
-    x = pd1['cdate'].to_numpy().reshape((-1, 1))
-    y = pd1['price'].to_numpy()
-    model = LinearRegression()
-    model.fit(x, y)
-    global slope_eth
-    slope_eth=str(model.coef_)
-
-def ltc_slope():
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * from coin_data WHERE cname = 'BITMEX:LTCUSD'")
-    myresult = mycursor.fetchall()
-    pd1 = pd.DataFrame(myresult, columns=['crid', 'cname', 'cdate', 'price', 'lows', 'highs'])
-    # line
-    pd1['cdate'] = pd.to_datetime(pd1['cdate'])
-    pd1['cdate'] = pd1['cdate'].apply(lambda x: x.toordinal())
-    x = pd1['cdate'].to_numpy().reshape((-1, 1))
-    y = pd1['price'].to_numpy()
-    model = LinearRegression()
-    model.fit(x, y)
-    global slope_ltc
-    slope_ltc=str(model.coef_)
-# CALLING THE FUNCS I CREATED SO WE HAVE ACCESS TO TEH GLOBAL OUTPUTS
-bltc_slope()
-eth_slope()
-ltc_slope()
+# ADDING TO PAGES FOR FLASK
 
 dash.register_page(__name__,)
-# def update_city_selected(input_value):
-    # return f'You selected: {input_value}'
+mycursor = mydb.cursor()
+mycursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES LIMIT 10 ")
+coinlist = mycursor.fetchall()
+coinlistdrop = []
+coinlistdrop2=[]
+for x in (coinlist):
+    # print(x)
+    # for i in x:
+    #     # print(i)
+    #     v=x[0]+" - "+x[1]
+# print(coinlistdrop)
+    # coinlistdrop.append(coinlist[x])
+    # print("table name:", x[0])
+    try:
+        mycursor.execute('SELECT * FROM ' + x[0] + ' LIMIT 1')
+        cname=mycursor.fetchall()
 
-# HTML COMPONENTS AND DISPLAYING SLOPES FOR EACH COIN
+        var=json.loads(cname[0][2])
+        if "/" in var:
+            var = var.split('/')[0]
+        # print("description:",var['description'])
+        coinlistdrop.append(x[0])
+        coinlistdrop2.append(x[0]+" -"+var['description'])
+    except:pass
+
+coinlistdrop=list(set(coinlistdrop))
+
+options = [{'label': coinlistdrop2[i], 'value':coinlistdrop[i]} for i in range(len(coinlistdrop2))]
+
+years=['2022','2021','2020','2019','2018','2017','2016','2015','2014','2013']
+months=['01','02','03','04','05','06','07','08','09','10','11','12']
+days=['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+# HTML COMPONENTS
 layout = html.Div([
-    html.H4('%%VERSUS'),
-    dcc.Graph(id="graph5", figure={"layout": {"height": 300, "width": 700}, }),
-    html.H4("LTC slope ="+slope_ltc),
-    html.H4("BLTC slope =" + slope_bltc),
-    html.H4("ETH slope =" + slope_eth),
-    # SUBMIT BUTTON SEEMS TO BE REQUIRED BY DASH BY I HONESTLY DONT USE IT
-    html.Div(
-            # [dcc.Input(id='input-box4', type='text'),
-             # dcc.Input(id='input-box5', type='text'),
-             html.Button('Submit', id='button-example-5')
-             # ]
-),
+    html.H4(children='Simple Linear Regression',
+            style={
+                'textAlign':'center'
+            }),
+    dcc.Graph(id="graph_4", figure={"layout": {"height": 300, "width": 700}, }),
 
+    html.Div(id='output-container-button',
+             children=''),
     html.Div(id='output-container-button5',
              children='Enter a value and press submit'),
+
+    dcc.Checklist(
+        id="checklist",
+        options=[
+            {'label': 'ETH', 'value': 'line1'},
+            {'label': 'BLTC', 'value': 'line2'},
+            {'label': 'LTC', 'value': 'line3'},
+            {'label': 'line4', 'value': 'line4'},
+            {'label': 'line5', 'value': 'line5'},
+            {'label': 'line6', 'value': 'line6'}
+
+        ],
+        labelStyle={'display': 'block'},
+        value=[]
+    ),
+    dcc.Dropdown(id="year_drop",multi=False,value='2022',options=years),
+    dcc.Dropdown(id="month_drop", multi=False, value='06', options=months),
+    dcc.Dropdown(id="day_drop", multi=False, value='21', options=days),
+    dcc.Dropdown(
+    options=options,
+    id='slr-dropdown',
+    value='bitmex_ethusd_eth',
+    placeholder="bitmex_ethusd_eth"
+),
+    dcc.Dropdown(
+    options=options,
+    id='slr-dropdown2',
+    value='bitmex_bltc',
+    placeholder="bitmex_bltc"
+),
+    dcc.Dropdown(
+    options=options,
+    id='slr-dropdown3',
+    value='bitmex_ltcusd',
+    placeholder="bitmex_ltcusd"
+),
+
+
+
 ])
-#REQUIRED DASH CALLBACK
+# DASH CALLBACK
 @dash.callback(
-    dash.dependencies.Output('graph5', 'figure'),
-    [dash.dependencies.Input('button-example-5', 'n_clicks')],
+    dash.dependencies.Output('graph_4', 'figure'),
+    dash.dependencies.Input('year_drop','value'),
+    dash.dependencies.Input('month_drop', 'value'),
+    dash.dependencies.Input('day_drop', 'value'),
+    [dash.dependencies.Input('checklist','value'),
+     [dash.dependencies.Input('slr-dropdown', 'value')],
+     [dash.dependencies.Input('slr-dropdown2', 'value')],
+     [dash.dependencies.Input('slr-dropdown3', 'value')],
 
+     ]
 )
-#CHART FUNC
-def update_output(n_clicks,):
+
+def update_output(year_drop,month_drop,day_drop,checklist,val,val2,val3):
+    # print ("----")
+    # print ( in1 )
+    # print ( date5 + "" + date6)
+    #
+    # print ("----")
+
     # print(" number of clicks", n_clicks)
-    mycursor = mydb.cursor(buffered=True)
+    # print(" value-->", value)
+    # VALUE IS FROM HTML FORM INPUT
+#    crypto_name = value
+    crypto_name = val[0]
+    print(crypto_name)
+    # print ("in1:", in1)
+    # if ( crypto_name == "" ):
+    #     crytoname = "BITMEX:ETHUSD_ETH"
 
-    mycursor.execute("SELECT * from coin_data limit 1000")
-    # CAN ADD OPTIONAL "LIMIT Z" COMMAND FOR A PRETTIER CHART OUTPUT
+    mycursor = mydb.cursor()
+    # QUERY DATA FROM DB
+    mycursor.execute("SELECT * from "+ crypto_name,)
     myresult = mycursor.fetchall()
-
-    pd1 = pd.DataFrame(myresult, columns=['id', 'cname', 'cdate', 'price', 'lows', 'highs'])
-    # print(pd1)
-    pd1['cdate'] = pd.to_datetime(pd1['cdate'])
-
-
-
-
-    # GROUPING DATA BY COIN NAME
-    groups = pd1.groupby(by='cname')
-    # EMPTY ARRAY TO APPEND RESULTS TO
-    data = []
-    # COLORS FOR OUR LINES
-    colors = ['red', 'blue', 'green']
-
-    for group, dataframe in groups:
+    # print(myresult)
+    # SETTING COLLUMNS FOR DF
+    pd1 = pd.DataFrame(myresult, columns=['crid', 'cdate', 'price',])
+    price_data=[]
+    for f in pd1.price:
+        d=json.loads(f)
+        price_data.append(d['low'])
+    # line
 
 
+    pd.set_option('display.max_rows', None)
 
-        # SORTING VALS BY DATE
-        dataframe = dataframe.sort_values(by=['cdate'])
-        # CREATING P_CHANGE COLLUMN,GROUPING BY CNAME AND APPLYING FUNC TO PRICE
-        dataframe['p_change']=dataframe.groupby('cname')['price'].pct_change(-1)
-        # print("pd1",pd1)
-        # print("dataframe",dataframe)
-        trace = go.Scatter(x=dataframe.cdate.tolist(),
-                       y=dataframe.p_change.tolist(),
-                       marker=dict(color=colors[len(data)]),
-                       name=group)
-        data.append(trace)
-        # print(data)
-    # CHART FEATURES
-    layout = go.Layout(xaxis={'title': 'Time'},
-                   yaxis={'title': 'Percent Change'},
-                   margin={'l': 40, 'b': 40, 't': 50, 'r': 50},
-                   hovermode='x')
+    # fig = go.Figure()
+    # fig.add_trace(go.Scatter(x=pd1.cdate, y=pd1.lows))
 
-    figure = go.Figure(data=data, layout=layout)
-    # print('processing figure...')
-    figure.update_layout(
-        plot_bgcolor='white',
-        paper_bgcolor='white',
-        font_color='black'
-    )
-    return figure
-# figure.show()
-# ??figure displays but the next print statement never gets reached
-#     print('after .show')
+    ## add second line ##########################################
+
+    # print("in2:", in2)
+    # if (crypto_name == "None"):
+    #     crytoname = "BITMEX:ETHUSD_ETH"
+
+    crypto_name2 = val2[0]
+    print("yuur",crypto_name2)
+    # print ("in1:", in1)
+    # if ( crypto_name == "" ):
+    #     crytoname = "BITMEX:ETHUSD_ETH"
+
+    mycursor2 = mydb.cursor()
+    # QUERY DATA FROM DB
+    mycursor2.execute("SELECT * from "+ crypto_name2,)
+    myresult2 = mycursor2.fetchall()
+    # print(myresult)
+    # SETTING COLLUMNS FOR DF
+    pd2 = pd.DataFrame(myresult2, columns=['crid', 'cdate', 'price',])
+    price_data2=[]
+    for w in pd2.price:
+        d=json.loads(w)
+        price_data2.append(d['low'])
+
+    # print ( pd2.price)
+
+    ## add second line ##########################################
+    crypto_name3 = val3[0]
+    print("y",crypto_name3)
+
+    # print ("in1:", in1)
+    # if ( crypto_name == "" ):
+    #     crytoname = "BITMEX:ETHUSD_ETH"
+
+    mycursor3 = mydb.cursor()
+    # QUERY DATA FROM DB
+    mycursor3.execute("SELECT * from "+ crypto_name3,)
+    myresult3 = mycursor3.fetchall()
+    # print(myresult)
+    # SETTING COLLUMNS FOR DF
+    pd3 = pd.DataFrame(myresult3, columns=['crid', 'cdate', 'price',])
+    price_data3=[]
+    for z in pd3.price:
+        d=json.loads(z)
+        price_data3.append(d['low'])
+
+
+    # print(pd3.cdate)
+    # print(price_data)
+    # data=[[price_data],[price_data2],[price_data3]]
+    print(len(pd1),len(pd2),len(pd3))
+    data={
+        crypto_name:price_data,
+        crypto_name2:price_data2,
+        crypto_name3:price_data3
+    }
+    # labs=[crypto_name,crypto_name2,crypto_name3]
+    pd4=pd.DataFrame(data=data,)
+    # print(pd4)
+    # pd4= pd.concat([pd1.cdate,price_data,price_data2,price_data3],axis=1)
+    p_change=pd4[crypto_name].pct_change()
+    # print(p_change)
+    p_change2=pd4[crypto_name2].pct_change()
+    p_change3=pd4[crypto_name3].pct_change()
+
+    # print(pd4)
+    # print(pd2.price)
+    # print("!!!",price_data,)
+    # print("@@@",price_data2,)
+    # print("###",price_data3,)
+
+    # print(p_change3)
+    fig=px.scatter(x=pd1['cdate'], y=[p_change,p_change2,p_change3], trendline='ols')
+    # fig.update_layout(
+    #     plot_bgcolor='rgba(0,0,0,0)',
+    #     paper_bgcolor='rgba(0,0,0,0)',
+    #     font_color='WHITE'
+    # )
+
+    # update figure on checkbox toggle
+    # fig.update_traces(marker_color="RoyalBlue",
+    #                   selector=dict(marker_color="MediumPurple"))
+
+    print(checklist)
+    # allowing users to hide and unhide traces for better view using checklist
+    if checklist == ['line1']:
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'ETH'}))
+    elif checklist==['line2']:
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'BLTC'}))
+    elif checklist==['line3']:
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'p_change3'}))
+    elif checklist==['line1','line2']:
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'ETH'}))
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'BLTC'}))
+    elif checklist==['line2','line1']:
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'ETH'}))
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'BLTC'}))
+    elif checklist==['line1','line3']:
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'ETH'}))
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'BLTC'}))
+    elif checklist==['line3','line1']:
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'ETH'}))
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'LTC'}))
+    elif checklist==['line2','line3']:
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'LTC'}))
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'BLTC'}))
+    elif checklist==['line3','line2']:
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'LTC'}))
+        fig.update_traces(overwrite=True, marker={"opacity": 0},selector=({'name':'BLTC'}))
+
+    return fig
+    # return p1
